7629ff604764b9b75f5210593ecca91269e4db5a
Mikhail Mikhailov <mikhailoff@inbox.ru>
2019-01-31 14:43:47 +0300

    ADD: Edit Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,12 +15,26 @@ class ArticlesController < ApplicationController
 	#@article = Article.new(params.require(:article).permit(:title, :text))
 	@article = Article.new(article_params)
 	
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+	  if @article.save
+	    redirect_to @article
+	  else
+	    render 'new'
+	  end
+
   end
 
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+	  @article = Article.find(params[:id])
+	 
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
   end
 
   def show
